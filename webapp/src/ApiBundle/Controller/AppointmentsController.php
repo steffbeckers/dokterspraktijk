812074fd21fd52be2f83798d488f1ad8a16c548f
Steff Beckers <steff@steffbeckers.eu>
2016-01-11 09:13:17 +0100

    API basis toegevoegd

---
+++ webapp/src/ApiBundle/Controller/AppointmentsController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steff
+ * Date: 05/01/16
+ * Time: 11:47
+ */
+
+namespace ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\View;
+
+class AppointmentsController extends Controller
+{
+    public function getAppointmentsAction()
+    {} // "get_appointments"     [GET] /api/appointments
+}
---
+++ webapp/src/ApiBundle/Entity/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ApiBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    //protected $apiKey;
+
+    public function __construct()
+    {
+        parent::__construct();
+        //$this->apiKey = $key;
+    }
+}
