7a730f807afcabe006c3c6d752442c320ae73fe0
Steff Beckers <steff@steffbeckers.eu>
2016-01-25 10:19:15 +0100

    Kalender aangepast

---
+++ webapp/src/AppBundle/Entity/AppSettings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steff
+ * Date: 25/01/16
+ * Time: 08:52
+ */
+
+namespace AppBundle\Entity;
+
+
+class AppSettings
+{
+    const APP_NAME = "Dokterspraktijk";
+}
