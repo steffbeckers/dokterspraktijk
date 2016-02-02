d6fd31aba2cc554448c177a3985a2592f8ff9361
YannickLemmens <lemmens.9@gmail.com>
2016-02-02 23:52:19 +0100

    added room entity

---
+++ webapp/src/AppBundle/Entity/Room.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Groups;
+
+/**
+ * Appointment
+ *
+ * @ORM\Table(name="room")
+ * @ORM\Entity
+ *
+ */
+class Room
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $roomId;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="room", type="string", length=50, nullable=true)
+     */
+    private $roomName;
+
+    /**
+     * Get roomId
+     *
+     * @return integer
+     */
+    public function getRoomId()
+    {
+        return $this->roomId;
+    }
+
+    /**
+     * Set roomName
+     *
+     * @param string $roomName
+     *
+     * @return Room
+     */
+    public function setRoomName($roomName)
+    {
+        $this->roomName = $roomName;
+
+        return $this;
+    }
+
+    /**
+     * Get roomName
+     *
+     * @return string
+     */
+    public function getRoomName()
+    {
+        return $this->roomName;
+    }
+}
