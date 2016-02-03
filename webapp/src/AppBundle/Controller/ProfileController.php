ffea3ce06eecdf50a5084df50fb15b52dc6fdb5f
StevenPeeters <steven.peeters@student.pxl.be>
2016-02-03 15:30:13 +0100

    Tegels op home pagina goed zetten

--- webapp/src/AppBundle/Controller/ProfileController.php
+++ webapp/src/AppBundle/Controller/ProfileController.php
@@ -56,9 +56,9 @@ class ProfileController extends Controller
     {
         if ( $this->getUser() != null)
         {
-        $id = $this->getUser()->getId();
-        $appointments = $this->getDoctrine()->getRepository('AppBundle:Appointment')->findBy(array('patientid' => $id));
-        return $this->render('Profile/showUserAppointments.html.twig', array('appointments' => $appointments));
+            $id = $this->getUser()->getId();
+            $appointments = $this->getDoctrine()->getRepository('AppBundle:Appointment')->findBy(array('patientid' => $id));
+            return $this->render('Profile/showUserAppointments.html.twig', array('appointments' => $appointments));
         } else {
             return $this->redirectToRoute("aanmelden");
         }
